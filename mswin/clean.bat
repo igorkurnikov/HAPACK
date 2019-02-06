2fd9182aad33951f6d3a06f93424df3153b07248
Nikolay Simakov <ns@compmodel.org>
2019-02-05 19:26:19 -0500

    added clean.bat to clean builds but leaving dependencies

---
+++ mswin/clean.bat
@@ -0,0 +1,26 @@
+rem clean all builds but leave python and other dependencies
+SET script_path=%~dp0
+cd %script_path%
+
+echo off
+
+if exist "obj" (
+    rmdir  /Q /S "%%D\scripts"
+)
+
+FOR %%D IN (PY3_Debug_Win32 PY3_Release_Win32 PY3_Debug_x64 PY3_Release_x64 Release_Win32 Release_x64) DO (
+    if exist "%%D" (
+        echo "Cleaning %%D"
+        if exist "%%D\scripts" (
+            rmdir  /Q /S "%%D\scripts"
+        )
+        del  /F /Q "%%D\*.lib"
+        del  /F /Q "%%D\*.a"
+        FOR %%F IN (*.lib harlem.exe harlem.exp harlem.ilk harlem.lib harlem.pdb) DO (
+            if exist "%%D\%%F" (
+                del  /F /Q "%%D\%%F"
+            )
+        )
+    )
+)
+
