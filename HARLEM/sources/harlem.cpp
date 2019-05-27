32f12c69a05cc3f74e2b1639333a735aee817af3
Igor Kurnikov <ivkurnikov@gmail.com>
2019-05-27 17:51:14 -0400

    Passed command arguments to Python

--- HARLEM/sources/harlem.cpp
+++ HARLEM/sources/harlem.cpp
@@ -1,5 +1,7 @@
 #include <windows.h>
 #include <iostream>
+#include <vector>
+#include <string>
 
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
@@ -55,9 +57,25 @@ int start_harlem(int argc, char **argv)
 	Py_SetProgramName(program);  /* optional but recommended */
 	Py_Initialize();
 
+	std::vector<std::wstring> python_args;
+	python_args.resize(argc);
+	int i;
+	wchar_t** argv_p = (wchar_t**)malloc(argc * sizeof(wchar_t*));
+	for (i = 0; i < argc; i++)
+	{
+		std::string tmp(argv[i]);
+		python_args[i].resize(tmp.length());
+		std::copy(tmp.begin(), tmp.end(), python_args[i].begin());
+		argv_p[i] = (wchar_t*)python_args[i].c_str();
+	}
+
+	PySys_SetArgv(argc, argv_p);
+
 	PyRun_SimpleString(
 		"from harlempy.start_harlem import start_harlem\n"
 		"start_harlem()\n");
+
+	delete[] argv_p;
 
 	if (Py_FinalizeEx() < 0) {
 		exit(120);
