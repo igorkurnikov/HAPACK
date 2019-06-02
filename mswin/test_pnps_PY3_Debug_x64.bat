f1cf759944b5e99eddbb46529d0ff78653593114
Nikolay Simakov <ns@compmodel.org>
2019-06-02 01:39:59 -0400

    pnps: added two bat files to run pnps regtests on windows

---
+++ mswin/test_pnps_PY3_Debug_x64.bat
@@ -0,0 +1,21 @@
+@echo off
+SET initial_path=%cd%
+SET script_path=%~dp0
+cd %script_path%
+
+SET config=PY3_Debug_x64
+
+REM Make directories for testing
+if exist "%script_path%%config%\pnpstests\" (
+    echo "%script_path%%config%\pnpstests already exists, will delete it and create again"
+    rmdir "%script_path%%config%\pnpstests" /s /q
+    mkdir "%script_path%%config%\pnpstests"
+) else (
+    mkdir "%script_path%%config%\pnpstests"
+)
+
+cd "%script_path%%config%\pnpstests"
+
+%script_path%%config%\python_d.exe -m pytest C:\MYPROG\HAPACK\PNPS\regtests
+
+cd %initial_path%
