8ec996cc25505218dd13dc5032e859c7aa736948
Igor Kurnikov <igor.kurnikov@interxinc.com>
2019-10-06 11:15:33 -0700

    Added PDB -> HIN converter

---
+++ examples/STRUCT/convert_pdb_to_hin.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+if(len(sys.argv) < 3):
+  print("usage: " + sys.argv[0] + "  inp.pdb  out.hin ")
+  exit(1)
+from harlempy import *
+fn_inp = sys.argv[1]
+fn_out = sys.argv[2]
+mset = HaMolSet()
+mset.LoadPDBFile(fn_inp)
+mset.SaveHINFile(fn_out)
+print("Done!")
