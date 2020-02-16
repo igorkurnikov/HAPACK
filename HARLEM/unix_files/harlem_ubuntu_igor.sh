239d042a86834f6dfdcb64b519bf585c56822cf2
Igor Kurnikov <ivkurnikov@gmail.com>
2020-02-16 10:13:16 -0500

    Simplify main harlem script in linux

--- HARLEM/unix_files/harlem_ubuntu_igor.sh
+++ HARLEM/unix_files/harlem_ubuntu_igor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export HARLEM_HOME="/share/apps/HARLEM"
-export PYTHONPATH="/share/apps/InterX_Simulations/Python3.7:$PYTHONPATH"
+export PYTHONPATH="$HARLEM_HOME:$PYTHONPATH"
 python3 -m harlempy $@
