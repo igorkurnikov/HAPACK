a1f7f54cda0f8dd17f8845445d5ea72b39336525
Igor Kurnikov <ivkurnikov@gmail.com>
2020-09-20 11:09:02 -0400

    renamed directory for linux scripts

--- HARLEM/unix_files/harlem_ubuntu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export HARLEM_HOME="/share/apps/HARLEM"
-export PYTHONPATH="/share/apps/InterX_Simulations/Python3.7:$PYTHONPATH"
-export LD_LIBRARY_PATH="/share/apps/anaconda3/lib/python3.7/site-packages/wx:$LD_LIBRARY_PATH"
-#export LD_LIBRARY_PATH="/share/apps/intel/mkl/lib/intel64_LIN:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
-python3 -m harlempy.start_harlem 
--- HARLEM/unix_files/harlem_ubuntu_igor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export HARLEM_HOME="/share/apps/HARLEM"
-export PYTHONPATH="$HARLEM_HOME:$PYTHONPATH"
-python3 -m harlempy $@
