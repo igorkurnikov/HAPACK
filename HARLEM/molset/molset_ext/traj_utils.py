14e35f6a6ecfe8bdd7e94e145ae2d0b7f8ba97fa
Igor Kurnikov <ivkurnikov@gmail.com>
2021-01-21 19:35:59 -0500

    Added an example of saving to PDB files

--- HARLEM/molset/molset_ext/traj_utils.py
+++ HARLEM/molset/molset_ext/traj_utils.py
@@ -1,20 +1,36 @@
 import os
 import shutil
 import tempfile
-import numpy as np
+NUMPY_IMPORTED = 0
+MDTRAJ_IMPORTED = 0
+try:
+    import numpy as np
+    NUMPY_IMPORTED = 1
+except:
+    pass
+#    print("numpy is not available - some functionaliy will be turned off ")
+
+try:
+    import mdtraj as md
+    MDTRAJ_IMPORTED = 1
+except:
+    pass
+#    print("mdtraj is not available - some functionaliy will be turned off ")
+
 import molset
-import mdtraj as md
 
 def MolSet_to_mdtraj_top( mset : molset.MolSet ):
-   temp_dir = tempfile.mkdtemp(prefix='molset_temp_')
-   print("temp_dir =",temp_dir)
-   temp_fname = os.path.join(temp_dir,"temp.pdb")
-   ires = mset.SavePDBFile(temp_fname)
-   topology = md.load_pdb(temp_fname).topology
-   if(os.path.exists(temp_dir)): shutil.rmtree(temp_dir)
-   return topology
+    if( MDTRAJ_IMPORTED == 0 ): return null
+    temp_dir = tempfile.mkdtemp(prefix='molset_temp_')
+    print("temp_dir =",temp_dir)
+    temp_fname = os.path.join(temp_dir,"temp.pdb")
+    ires = mset.SavePDBFile(temp_fname)
+    topology = md.load_pdb(temp_fname).topology
+    if(os.path.exists(temp_dir)): shutil.rmtree(temp_dir)
+    return topology
 
-def MolSet_crd_from_frame( mset : molset.MolSet, t : md.Trajectory ):
+def MolSet_crd_from_frame( mset : molset.MolSet, t ):
+    if( MDTRAJ_IMPORTED == 0 ): return null
     print(t.time)
     for i,at in enumerate(mset):
         print("i =",i)
---
+++ examples/MD/save_crd_pdb.py
@@ -0,0 +1,32 @@
+#
+#	This script is used to save Harlem file format snapshots (.hlm) along the MD 
+#  trajectory. These files are saved in the same directory as your molecule files. 
+#
+
+if(script_status == SCRIPT_START): 
+# Code to initialize the script on the trajectory
+  
+  pmset = GetCurMolSet()
+  i = 0 # Sets a label "i" beginning at 0.
+  
+
+elif(script_status == SCRIPT_STOP):
+#  Code to end the script after MD trajectory is finished.
+  
+  print("\nTrajectory Playback Completed\n")
+  print("PDB snapshots saved to file directory")
+
+
+else: 
+#  Actual code that saves the PDB files.
+
+    i = i+1 # Increments label by a value for each file name.
+    
+    if(i % 10 == 0):
+	  #	The value used with the modulus for "i" determines how often to take a 
+	  #  snapshot. Here it is set to take a snapshot every 100 MD steps.
+	  #  Change this value as desired.
+	  	  
+	  fname = "snap" + str(10000+i) + ".pdb" #  Sets the file name of the snapshot
+	  print("Snapshot saved to ", fname)
+	  pmset.SavePDBFile(fname) # Writes the PDB file
