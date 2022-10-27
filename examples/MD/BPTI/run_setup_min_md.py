f7099fbc10139a5115a54a31a483e720813f0371
igorkurnikov <60519686+igorkurnikov@users.noreply.github.com>
2022-10-27 13:37:50 -0400

    Added PKa calculations and RMSD calculations to BPTI MD example script

--- examples/MD/BPTI/run_setup_min_md.py
+++ examples/MD/BPTI/run_setup_min_md.py
@@ -4,10 +4,19 @@ mset = MolSet()
 mset.LoadPDBFile("6PTI.pdb")   # Load Protein atomic coordinates
 
 medit = MolEditor()
-medit.DeleteExtraAtoms(mset)   # Delete aotoms not found in residue templates 
+medit.DeleteExtraAtoms(mset)   # Delete atoms not found in residue templates 
 medit.AddMissingAtoms(mset)    # Add Hydrogens and other missing atoms from residue templates
 medit.FixBondsUsingTempl(mset) # Fix bonds that may incorrectly be build when loading PDB
 medit.Solvate(mset)            # Solvate molecule in water box 
+
+# Compute pKa using Continuum Electrostatics model and set atomic charges acccording to pH:
+
+prot_mod = mset.GetProtonRedoxMod(True)
+pApp = GetHarlemApp()
+pApp.RasMolCmd("select protein")
+prot_mod.CalcPKaForSelection(True)
+prot_mod.SetPH(6.0)
+prot_mod.SetChargesForCurrentPH()
 
 # Energy minimization
 
@@ -29,5 +38,15 @@ mm_mod.SetWrtLogFreq(10)
 mm_mod.SaveAllInpFiles()
 mm_mod.Run()                         # Run Molecular Dynamics 
 
-# Compute RMSD of the trajectory 
+# Compute RMSD of CA atoms along the MD trajectory 
+
+trj_anal_mod = mm_mod.GetTrajAnalMod(True)   # Get MD trajectory analysis module
+rmsd_ag = trj_anal_mod.GetRMSDAgent(True);   # Get an Agent  to compute RMSD along MD trajectory
+pApp.RasMolCmd("define CA_ATOMS *.CA")       # define Atom Group with C-alpha atoms of the protein
+rmsd_ag.SetAtomsFit("CA_ATOMS");
+rmsd_ag.SetAtomsRMSD("CA_ATOMS")
+trj_anal_mod.AnalyzeTrajectory(True)         # Analyse MD trajectory computing RMSD of CA_ATOMS 
+
+
+
 
